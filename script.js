386ff920515503b0243b755df7ff9d64a8f5c54e
AYESHAJUMAID <abdulhajunaid@gmail.com>
2024-03-09 10:01:25 +0500

    done

---
+++ script.js
@@ -0,0 +1,53 @@
+var literalNotation = [1, 2, 4, 6, 8, 9];
+console.log(literalNotation);
+
+var objectNotation = {
+  1: "one",
+  2: "two",
+  4: "four",
+  6: "six",
+  8: "eight",
+  9: "nine",
+};
+console.log(objectNotation);
+
+var initialize = ["abdullah", "adil", "amna", "ayesha", "abaan"];
+console.log(initialize);
+
+var number = [23, 54, 67, 98, 98, 54, 87];
+console.log(number);
+
+var boolean = ["true", "false", "true", "true", "false"];
+console.log(boolean);
+
+var mixed = [3, 5, "one", "five", 44, 10, "true", "false"];
+console.log(mixed);
+
+var qualification = [
+  "SSC",
+  "HSC",
+  "BSC",
+  "BS",
+  "BCOM",
+  "MS",
+  "M.phil.,",
+  "PHD",
+];
+document.write(
+  qualification[0],
+  "<br>",
+  qualification[1],
+  "<br>",
+  qualification[2],
+  "<br>",
+  qualification[3],
+  "<br>",
+  qualification[4],
+  "<br>",
+  qualification[5],
+  "<br>",
+  qualification[6],
+  "<br>",
+  qualification[7],
+  "<br>"
+);
